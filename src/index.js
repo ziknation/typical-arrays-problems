9763ce586c77b3f13a6046c2a78f483d61c71a84
Yuri Zikeev <zikeev.yura@gmail.com>
2020-10-01 13:56:04 +0300

    feat: passed all tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (array == undefined) return 0;
+    if (array.length === 1) return array[0];
+    if (array.length >= 2){
+      let min = array[0];
+      for (let i = 1; i <= array.length - 1; i++){
+        if (array[i] < min){
+          min = array[i];
+        }
+      }
+      return min;
+    }
+    return 0;
+  }
+  
+  exports.max = function max (array) {
+    if (array == undefined) return 0;
+    if (array.length === 1) return array[0];
+    if (array.length >= 2){
+      let max = array[0];
+      for (let i = 1; i <= array.length - 1; i++){
+        if (array[i] > max){
+          max = array[i];
+        }
+      }
+      return max;
+    }
+    return 0;
+  }
+  
+  exports.avg = function avg (array) {
+    if (array == undefined) return 0;
+    if (array.length >= 1){
+      let sum = 0;
+      for (let i = 0; i <= array.length - 1; i++){
+        sum += array[i];
+      }
+      let avg = sum / array.length;
+      return avg;   
+    }
+    return 0;
+  }
+  
